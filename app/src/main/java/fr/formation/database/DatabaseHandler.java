a6c9c8212e93d1dac018014a5ee32d3bc6c5d5f5
Formation <stagiaireA@mail.com>
2018-01-08 15:16:29 +0100

    Correction de bug - Supression d'un underscore

--- app/src/main/java/fr/formation/database/DatabaseHandler.java
+++ app/src/main/java/fr/formation/database/DatabaseHandler.java
@@ -15,7 +15,7 @@ public class DatabaseHandler extends SQLiteOpenHelper{
     private static final int DATABASE_VERSION = 1;
 
     private static final String CONTACT_TABLE_SQL = "CREATE TABLE contacts(" +
-                                                        "id INTEGER PRIMARY KEY AUTO_INCREMENT," +
+                                                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                                                         "first_name TEXT, " +
                                                         "email TEXT NOT NULL )";
 
