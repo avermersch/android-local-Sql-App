4efac0f624335f5297f90591a9e3097312b2a984
Formation <stagiaireA@mail.com>
2018-01-08 16:26:16 +0100

    Ajout d'un actionbar dans FormActivity

--- app/src/main/java/com/example/formation/localsqlapp/FormActivity.java
+++ app/src/main/java/com/example/formation/localsqlapp/FormActivity.java
@@ -1,5 +1,6 @@
 package com.example.formation.localsqlapp;
 
+        import android.app.ActionBar;
         import android.content.ContentValues;
         import android.database.sqlite.SQLiteException;
         import android.support.v7.app.AppCompatActivity;
@@ -24,6 +25,11 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
+        ActionBar actionBar = getActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         //récupération zone d'affichage
         textViewNom = (TextView)findViewById(R.id.textView);
         textViewPrenom = (TextView)findViewById(R.id.textView1);
@@ -35,9 +41,9 @@ public class FormActivity extends AppCompatActivity {
     public void onValid(View v) {
         Button clickedButton = (Button) v;
         //Recuperation de la saisie de l'utilisateur
-        String editTextNom = ((EditText) findViewById(R.id.editTextNom)).toString();
-        String editTextPrenom = ((EditText) findViewById(R.id.editTextPrenom)).toString();
-        String editTextEmail = ((EditText) findViewById(R.id.editTextEmail)).toString();
+        String editTextNom = ((EditText) findViewById(R.id.editTextNom)).getText().toString();
+        String editTextPrenom = ((EditText) findViewById(R.id.editTextPrenom)).getText().toString();
+        String editTextEmail = ((EditText) findViewById(R.id.editTextEmail)).getText().toString();
 
         //Instanciation de la base de données
         DatabaseHandler db = new DatabaseHandler(this);
@@ -52,10 +58,14 @@ public class FormActivity extends AppCompatActivity {
         try {
             db.getWritableDatabase().insert("contacts", null, insertValues);
             Toast.makeText(this, "Insertion OK", Toast.LENGTH_SHORT).show();
+            ((EditText) findViewById(R.id.editTextNom)).setText("");
+            ((EditText) findViewById(R.id.editTextPrenom)).setText("");
+            ((EditText) findViewById(R.id.editTextEmail)).setText("");
 
         }
         catch (SQLiteException ex) {
             Log.e("SQL EXCEPTION", ex.getMessage());
+            Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
         }
     }
 }
