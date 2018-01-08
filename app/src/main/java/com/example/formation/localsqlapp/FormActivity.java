8efd2721184ce152a7b2a6824ba17a651d63803d
Formation <stagiaireA@mail.com>
2018-01-08 15:11:16 +0100

    Recuperation database sur FormActivity

--- app/src/main/java/com/example/formation/localsqlapp/FormActivity.java
+++ app/src/main/java/com/example/formation/localsqlapp/FormActivity.java
@@ -1,20 +1,59 @@
 package com.example.formation.localsqlapp;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import fr.formation.database.DatabaseHandler;
 
 public class FormActivity extends AppCompatActivity {
+    private TextView textViewNom;
+    private TextView textViewPrenom;
+    private TextView textViewEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        //récupération zone d'affichage
+        textViewNom = (TextView)findViewById(R.id.textView);
+        textViewPrenom = (TextView)findViewById(R.id.textView1);
+        textViewEmail = (TextView)findViewById(R.id.textView2);
+
+
     }
 
     public void onValid(View v) {
         Button clickedButton = (Button) v;
+        //Recuperation de la saisie de l'utilisateur
+        String editTextNom = ((EditText) findViewById(R.id.editTextNom)).toString();
+        String editTextPrenom = ((EditText) findViewById(R.id.editTextPrenom)).toString();
+        String editTextEmail = ((EditText) findViewById(R.id.editTextEmail)).toString();
 
+        //Instanciation de la base de données
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        //Définition desw données à insérer
+        ContentValues insertValues = new ContentValues();
+        insertValues.put("first_name", editTextNom);
+        insertValues.put("name", editTextPrenom);
+        insertValues.put("email", editTextEmail);
+
+        //Insertion des données
+        try {
+        db.getWritableDatabase().insert("contacts", null, insertValues);
+
+        }
+        catch (SQLiteException ex) {
+                Log.e("SQL EXCEPTION", ex.getMessage());
+        }
     }
 }
---
+++ app/src/main/java/fr/formation/database/DatabaseHandler.java
@@ -0,0 +1,38 @@
+package fr.formation.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Formation on 08/01/2018.
+ */
+
+public class DatabaseHandler extends SQLiteOpenHelper{
+
+
+    private static final String DATABASE_NAME = "contact_database";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String CONTACT_TABLE_SQL = "CREATE TABLE contacts(" +
+                                                        "id INTEGER PRIMARY KEY AUTO_INCREMENT," +
+                                                        "first_name TEXT, " +
+                                                        "email TEXT NOT NULL )";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CONTACT_TABLE_SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //supprime la table si existe
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS contacts");
+        //table re créer
+        this.onCreate(sqLiteDatabase);
+    }
+}
