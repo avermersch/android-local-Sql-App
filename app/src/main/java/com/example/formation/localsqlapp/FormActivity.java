124c6664e5e63efd831356c745af9e89b9a1a28a
Formation <stagiaireA@mail.com>
2018-01-08 15:15:08 +0100

    Insertion d'un contact dans la base SQLite

--- app/src/main/java/com/example/formation/localsqlapp/FormActivity.java
+++ app/src/main/java/com/example/formation/localsqlapp/FormActivity.java
@@ -1,16 +1,17 @@
 package com.example.formation.localsqlapp;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteException;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+        import android.content.ContentValues;
+        import android.database.sqlite.SQLiteException;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import fr.formation.database.DatabaseHandler;
+        import fr.formation.database.DatabaseHandler;
 
 public class FormActivity extends AppCompatActivity {
     private TextView textViewNom;
@@ -49,11 +50,12 @@ public class FormActivity extends AppCompatActivity {
 
         //Insertion des données
         try {
-        db.getWritableDatabase().insert("contacts", null, insertValues);
+            db.getWritableDatabase().insert("contacts", null, insertValues);
+            Toast.makeText(this, "Insertion OK", Toast.LENGTH_SHORT).show();
 
         }
         catch (SQLiteException ex) {
-                Log.e("SQL EXCEPTION", ex.getMessage());
+            Log.e("SQL EXCEPTION", ex.getMessage());
         }
     }
 }
